57758d5f9e4a233166b4f52555dd0c18ac193668
jjw213 <ssvsxx@naver.com>
2022-04-03 21:46:08 +0900

    board

--- src/main/java/hello/hellospring/service/BoardService.java
+++ src/main/java/hello/hellospring/service/BoardService.java
@@ -1,12 +1,7 @@
 package hello.hellospring.service;
 
 import hello.hellospring.domain.BoardDTO;
-import hello.hellospring.domain.Member;
 import hello.hellospring.repository.BoardRepository;
-import hello.hellospring.repository.MemberRepsitory;
-import hello.hellospring.repository.MemoryBoardRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,7 +12,8 @@ public class BoardService {
     public BoardService(BoardRepository boardRepository) {
         this.boardRepository = boardRepository;
     }
-    public Integer save(BoardDTO boardDTO){
+
+    public Integer save(BoardDTO boardDTO) {
         boardRepository.save(boardDTO);
 
         return boardDTO.getIdx();
